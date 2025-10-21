c63ac25ebffc81ea6f5b554e8da6c6dd16fbcc5d
suwangying <suwangying619@gmail.com>
2025-10-21 19:15:21 -0400

    comment

--- app/src/main/java/com/example/ma_final_project/MainActivity.kt
+++ app/src/main/java/com/example/ma_final_project/MainActivity.kt
@@ -1,54 +1,20 @@
 package com.example.ma_final_project
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import android.content.Intent
-import android.widget.EditText
-import android.widget.Toast
-
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        val btnLogin = findViewById<Button>(R.id.btnLogin)
-        val btnSignup = findViewById<Button>(R.id.btnSignup)
-        val forgotPasswd = findViewById<TextView>(R.id.tvForgotPassword)
-
-        val etPhone = findViewById<EditText>(R.id.etPhone)
-        val etPassword = findViewById<EditText>(R.id.etPassword)
-
-        // Login Button -> go to Home Page
-        btnLogin.setOnClickListener {
-
-            val phone = etPhone.text.toString().trim()
-            val password = etPassword.text.toString().trim()
-            if (phone.isNotEmpty() && password.isNotEmpty()) {
-                // TODO: You can also add actual authentication here (SQLite/Firebase)
-                val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
-                finish()
-            } else {
-                Toast.makeText(this, "Please enter phone number and password", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        // SignUp Button -> go to SignUp Page
-        btnSignup.setOnClickListener {
-            /*val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)*/
-        }
-
-        // Forgot Password Link -> go to Forgot Password Page
-        forgotPasswd.setOnClickListener {
-            /*val intent = Intent(this, ForgotPasswordActivity::class.java)
-            startActivity(intent)*/
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
